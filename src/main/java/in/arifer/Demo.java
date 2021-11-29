41d6f7fecee82b0654a9029f08dbc61d3c872961
marif36 <94030510+marif36@users.noreply.github.com>
2021-11-29 20:41:25 +0530

    Update Demo.java

--- src/main/java/in/arifer/Demo.java
+++ src/main/java/in/arifer/Demo.java
@@ -2,7 +2,6 @@ package in.arifer;
 
 public class Demo {
 	public static void main(String[] args) {
-		System.out.println("***Simple Maven Jenkins***");
+		System.out.println("***Simple Maven Program with Jenkins Job Creation***");
 	}
-
 }
